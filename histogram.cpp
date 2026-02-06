06853c14c098b458efd4f23155809e451ffa6eb3
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-02-05 21:43:17 -0700

    new function for parallel bin counting operation

--- histogram.cpp
+++ histogram.cpp
@@ -3,6 +3,17 @@
 #include <pthread.h>
 
 int thread_count ;
+
+struct ThreadArguments {
+    int my_rank;
+    int bin_count;
+    int data_count;
+    int* array;
+    double* bin_limits;
+    int* histogram;
+    pthread_mutex_t* mutex;
+};
+
 double* get_bin_limits(int min, int max, int bin_count);
 int main(){
     srand(100);
@@ -41,4 +52,37 @@ double* get_bin_limits(int min, int max, int bin_count){
         bin_limits[i] = min+ interval*i;
     }
     return bin_limits;
+}
+void*  bin_per_thread(void* arguments){
+    ThreadArguments* thread_args = (ThreadArguments*) arguments;
+    int my_rank = thread_args->my_rank;
+    int bin_count = thread_args->bin_count;
+    int data_count = thread_args->data_count;
+    int* array = thread_args->array;
+    double* bin_limits = thread_args->bin_limits;
+    int* histogram = thread_args-> histogram;
+    pthread_mutex_t* mutex = thread_args->mutex;
+
+    int* local_histogram = new int [bin_count]();
+    int el_per_thread = data_count/bin_count;
+    int first_idx = my_rank*el_per_thread;
+    int last_idx = first_idx+el_per_thread;
+
+    // Computing local histogram per thread
+    for (int i =first_idx; i<last_idx; i++){
+        for (int j=1; j<bin_count+1;j++){
+            if (array[i]<bin_limits[j]){
+                local_histogram[j-1]++;
+                break;
+            }
+        }
+    }
+    // Updating global histogram with mutex lock
+    pthread_mutex_lock(mutex);
+    for (int i=0; i<bin_count; i++){
+        histogram[i]+=local_histogram[i];
+    }
+    pthread_mutex_unlock(mutex);
+    delete[] local_histogram;
+    return NULL;
 }
