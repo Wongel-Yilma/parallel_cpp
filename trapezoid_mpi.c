2cfcca958aa737cd335ede6fdb0b16002510b46c
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-14 14:24:22 -0600

    MPI implementation of trapeziod integration scheme

---
+++ trapezoid_mpi.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <mpi.h>
+
+
+double f(double);
+double Trapz(int , double , double, double );
+int main(){
+    int my_rank, comm_sz, n=4096, local_n;
+    double a=0.0, b = 3.0, h, local_a, local_b;
+
+    double local_int, global_int;
+    int q;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&comm_sz );
+    h = (b-a)/n;
+    local_n = n/comm_sz;
+    local_a = a+local_n*h*my_rank;
+    local_b = local_a+h*local_n;
+    local_int = Trapz( local_n,  local_a,  local_b,  h);
+
+    if (my_rank!=0){
+        MPI_Send(&local_int, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }
+    else if (my_rank==0){
+        global_int = local_int;
+        for (int q=1; q<comm_sz;q++){
+            MPI_Recv(&local_int, 1, MPI_DOUBLE, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            global_int+=local_int;
+        }
+        printf("Total area: %f\n", global_int);
+    }
+    MPI_Finalize();
+    return 0;
+}
+
+double f(double x){
+    return x*x+2;
+}
+
+double Trapz(int local_n, double local_a, double local_b, double h){
+    double local_sum = (f(local_a)+f(local_b))/2;
+    for (int i=1; i<local_n; i++){
+        local_sum += f(local_a+h*i);
+    }
+    local_sum*=h;
+    return local_sum;
+}
+
+
+
