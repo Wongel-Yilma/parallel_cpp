bbce66c5d1056351831dd66c7e164c509954b34e
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-14 16:57:06 -0600

    Replace multiple send recieves with broadcast

--- trapezoid_mpi.c
+++ trapezoid_mpi.c
@@ -46,17 +46,13 @@ void Get_input(int my_rank, int comm_sz, double* a_p, double* b_p, int* n_p){
     if (my_rank==0){
         printf("Provide a, b and n.\n");
         scanf("%1lf %1lf %1d", a_p, b_p, n_p);
-        for (dest=1; dest<comm_sz; dest++){
-            MPI_Send(a_p, 1, MPI_DOUBLE, dest,0, MPI_COMM_WORLD);
-            MPI_Send(b_p, 1, MPI_DOUBLE, dest,0, MPI_COMM_WORLD);
-            MPI_Send(n_p, 1, MPI_INT, dest,0, MPI_COMM_WORLD);
-        }
+       
     }
-    else{
-        MPI_Recv(a_p, 1, MPI_DOUBLE, 0 ,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(b_p, 1, MPI_DOUBLE, 0 ,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(n_p, 1, MPI_INT, 0 ,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    MPI_Bcast(a_p, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(b_p, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+
 }
 
 
