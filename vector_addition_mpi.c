df6cd1f7ee0e36ff9b07b753a1d74e89bda8fb9a
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-14 19:12:30 -0600

    vector addition using MPI

---
+++ vector_addition_mpi.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+void Generate_vector(double*, int , int, int , MPI_Comm);
+void Add_vectors(double*, double*, double*, int);
+int main(){
+    int my_rank, comm_sz, n = 1024 , local_n,i;
+    double* local_a;
+    double* local_b;
+    double* local_c;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    local_n = n/comm_sz;
+    local_a = malloc(local_n*sizeof(double));
+    local_b = malloc(local_n*sizeof(double));
+    local_c = malloc(local_n*sizeof(double));
+
+    Generate_vector(local_a, local_n, n, my_rank, MPI_COMM_WORLD);
+    Generate_vector(local_b, local_n, n, my_rank, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    Add_vectors(local_a, local_b, local_c, local_n);
+    printf("Rank %d has %d number of elements\n", my_rank, local_n);
+
+    MPI_Finalize();
+    return 0;
+}
+void Generate_vector(double* local_a, int local_n, int n, int my_rank, MPI_Comm comm){
+    double* a =NULL;
+    int i ;
+    if (my_rank==0){
+        a = malloc(n*sizeof(double));
+        for (i=0; i<n; i++){
+            a[i] = (double)rand()/(RAND_MAX);
+        }
+        printf("Rank %d has created %d number of elements\n", my_rank,n);
+    }
+    MPI_Scatter(a, local_n, MPI_DOUBLE, local_a,local_n, MPI_DOUBLE, 0, comm);
+}
+void Add_vectors(double* a, double* b, double* c, int n){
+    int k ;
+    for (k=0; k<n; k++){
+        c[k] = a[k]+b[k];
+    }
+    
+}
+
+
